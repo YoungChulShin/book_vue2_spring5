b114cc81d0885a3940cc3ea9f6940af8c595a379
YoungChulShin <go1323@gmail.com>
2020-06-23 04:43:38 +0900

    Implementing payload class(WIP)

---
+++ taskagile/vuejs.spring-boot.mysql/src/test/java/com/taskagile/web/payload/RegistrationPayloadTests.java
@@ -0,0 +1,44 @@
+package com.taskagile.web.payload;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Set;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+
+public class RegistrationPayloadTests {
+
+    private Validator validator;
+
+    @Before
+    public void setup() {
+        final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    public void validate_blankPayload_shouldFail() {
+        RegistrationPayload payload = new RegistrationPayload();
+        Set<ConstraintViolation<RegistrationPayload>> violations = validator.validate(payload);
+        assertEquals(3, violations.size());
+    }
+
+    @Test
+    public void validate_payloadWithInvalidEmail_shouldFail() {
+        RegistrationPayload payload = new RegistrationPayload();
+        payload.setUsername("YoungChulShin");
+        payload.setPassword("TestPass");
+        payload.setEmailAddress("myEmail");
+
+        Set<ConstraintViolation<RegistrationPayload>> violations = validator.validate(payload);
+        assertEquals(1, violations.size());
+    }
+}
