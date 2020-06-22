b05b936e8a7795ca78459af7e764273854ac4f7e
영철 신 <youngchul.shin@meshkorea.net>
2020-06-22 13:34:36 +0900

    Implementing login data binding (WIP)

--- taskagile/vuejs.spring-boot.mysql/front-end/tests/unit/RegisterPage.spec.js
+++ taskagile/vuejs.spring-boot.mysql/front-end/tests/unit/RegisterPage.spec.js
@@ -1,16 +1,35 @@
-import Vue from 'vue'
+import { mount } from '@/vue/test-utils'
 import RegisterPage from '@/views/RegisterPage'
 
 describe('RegisterPage.vue', () => {
-  it ('should render correct contents', () => {
-    const Constructor = Vue.extend(RegisterPage);
-    const vm = new Constructor().$mount();
-    expect(vm.$el.querySelector('.logo').getAttribute('src')).toEqual('/static/images/logo.png');
-    expect(vm.$el.querySelector('.tagline').textContent).toEqual('Open source task management tool');
-    expect(vm.$el.querySelector('#username').value).toEqual('');
-    expect(vm.$el.querySelector('#emailAddress').value).toEqual('');
-    expect(vm.$el.querySelector('#password').value).toEqual('');
-    expect(vm.$el.querySelector('form button[type="submit"]').textContent).toEqual('Create account');
+
+  let wrapper
+  // let fieldUsername
+  // let fieldEmailAddress
+  // let fieldPassword
+  // let buttonSubmit
+
+  beforeEach(() => {
+    wrapper = mount(RegisterPage)
+    // fieldUsername = wrapper.find('#username')
+    // fieldEmailAddress = wrapper.find('#emailAddress')
+    // fieldPassword = wrapper.find('#password')
+    // buttonSubmit = wrapper.find('form button[type="submit"]')
   })
+
+  it ('should contain data model with initial values', () => {
+    expect(wrapper.vm.form.username).toEqual('')
+  })
+
+  // it ('should render correct contents', () => {
+  //   const Constructor = Vue.extend(RegisterPage);
+  //   const vm = new Constructor().$mount();
+  //   expect(vm.$el.querySelector('.logo').getAttribute('src')).toEqual('/static/images/logo.png');
+  //   expect(vm.$el.querySelector('.tagline').textContent).toEqual('Open source task management tool');
+  //   expect(vm.$el.querySelector('#username').value).toEqual('');
+  //   expect(vm.$el.querySelector('#emailAddress').value).toEqual('');
+  //   expect(vm.$el.querySelector('#password').value).toEqual('');
+  //   expect(vm.$el.querySelector('form button[type="submit"]').textContent).toEqual('Create account');
+  // })
 })
 
